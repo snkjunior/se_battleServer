172a194699a9395beaa756fa5ebd83514ef92c16
Oleg Goroun <oleg.goroun@levelupers.com>
2014-10-30 16:38:25 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var io = require("socket.io")(8000);
+
+var server = require("./lib/server");
+server.init(io.sockets);
+
+io.on('connection', function(socket) {
+    console.log("New client connected. Id: " + socket.id);
+    server.addUserAuthSocketEvents(socket);
+});
+
+
+
